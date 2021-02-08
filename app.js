7fad3d92980cffeedebdf9619d3b337e6e48e87c
Purna Gurung <purnagurung903@gmail.com>
2021-02-08 12:47:36 -0500

    added response to the promts for action

--- app.js
+++ app.js
@@ -18,6 +18,55 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-
-  
+// run start function after the connection is made to prompt the user
+  Start();
 })
+
+// function which prompts the user for what action they should take
+
+function start(){
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "Add employee",
+        "Add department",
+        "Add role",
+        "Update info",
+        "View info",
+        "Delete info",
+        "Exit"
+
+
+      ]
+    })// response to the action 
+    .then(function(answer){
+      if (answer.action === "Add employer"){
+        addEmployee();
+      }else if(answer.action === "Add department"){
+        addDepartment()
+      }else if(answer.action === "Add role"){
+        addRole()
+      }else if(answer.action === "Update info"){
+        myUpdate()
+      }else if(answer.action === "View info"){
+        myView()
+      }else if(answer.action === "Delete info"){
+        myDelete()
+      } else{
+        connection.end();
+      }
+
+       
+    
+}   )}     
+        
+       
+
+        
+
+
+      
+    
